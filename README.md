800783d2da85b6e58481cd524e4ca5985ebe32d8
Tanvir Ahmed <tanvir.mclv@gmail.com>
2018-03-27 15:03:57 -0700

    readme Introduction done

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # jsNinja
+## Notes and exercises from Secrets of the JavaScript Ninja-John Resig book
+
+###Chapter-1: Introduction
+
+First piece: We will extensively use assert() and also test codes that execute asynchronously.
+Second piece: We will do performance analysis (how fast) of the functions
+
+###Chapter-2: Testing and Debugging
+
+- Make sure your code works in all browsers.
+
+####Debugging
+
+- There are 2 important parts in to dubugging in JS: Logging statements and Breakpoint.
+
+
+
+
+
+
+
--- testing.js
+++ testing.js
@@ -1,4 +1,16 @@
 var assert = require('assert');
 
-assert( false, "This statement is true." );
-assert.equal(1,1);
+/**
+* If the first argument in the assert function is false
+* then, it throws an error with the statement passed as the second argument.
+*/
+assert( true, "This statement is true." ); // Doesn't do anything
+//assert( false, "This will never succeed." ); //Throws error
+
+/**
+*/
+perf("String Concatenation", function(){
+ var name = "John";
+ for ( var i = 0; i < 20; i++ )
+ name += name;
+});
