964abd675d6a353b5acbafc3b96580a00bf22b05
Tanvir Ahmed <tanvir.mclv@gmail.com>
2018-04-06 15:46:00 -0700

    prettified

--- designPattern.js
+++ designPattern.js
@@ -7,44 +7,44 @@
 */
 //Example of an IIFE
 console.log(function() {
-	return 5;
+  return 5;
 }());
 
 //Module design patter example. 
 //Created a module HTMLChanger which hides everything excepts the callChangeHTML method.
 var HTMLChanger = (function() {
-	var contents = 'content';
-	var changeHTML = function () {
-		//var element = document.getElementById('root');
-		//element.innerHTML = contents;
-	}
-	return {
-		callChangeHTML: function() {
-			changeHTML();
-			console.log(contents);
-		}
-	}
+  var contents = 'content';
+  var changeHTML = function () {
+    //var element = document.getElementById('root');
+    //element.innerHTML = contents;
+  }
+  return {
+    callChangeHTML: function() {
+      changeHTML();
+      console.log(contents);
+    }
+  }
 }());
 HTMLChanger.callChangeHTML(); //contents
 console.log(HTMLChanger.contents); //undefined
 
 //Revealing module pattern is same, except that you can specify what to make public.
 var Exposer = (function() {
-	var privateVar = 10;
-	var privateMethod = function() {
-		console.log('Inside a private method');
-		privateVar++;
-	}
-	var methodToExpose = function() {
-		console.log('This method will be exposed');
-	}
-	var anotherMethodIWantToExpose = function() {
-		privateMethod();
-	}
-	return {
-		first: methodToExpose,
-		second: anotherMethodIWantToExpose
-	}
+  var privateVar = 10;
+  var privateMethod = function() {
+    console.log('Inside a private method');
+    privateVar++;
+  }
+  var methodToExpose = function() {
+    console.log('This method will be exposed');
+  }
+  var anotherMethodIWantToExpose = function() {
+    privateMethod();
+  }
+  return {
+    first: methodToExpose,
+    second: anotherMethodIWantToExpose
+  }
 
 }());
 Exposer.first(); //gets output
